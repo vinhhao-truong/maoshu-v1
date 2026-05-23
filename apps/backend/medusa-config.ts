d0dbd9eb6a08d4da9dc88c56c0940b1407f9d2a5
Arnold Truong <vinhhao.truong.52@gmail.com>
2026-05-23 16:09:04 +1000

    fix backend build

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -28,7 +28,7 @@ module.exports = defineConfig({
       },
       {
         resolve: "@medusajs/medusa/workflow-engine-redis",
-        options: { redis: { url: process.env.REDIS_URL } },
+        options: { redisUrl: process.env.REDIS_URL },
       },
     ] : []),
     {
